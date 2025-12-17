2b1903a11e13c2f07e05e0f81a82a885776ca1af
shellipal <palshelli5@gmail.com>
2025-12-17 20:56:45 +0530

    variables

---
+++ 01_Basics/app.js
@@ -0,0 +1,42 @@
+// alert("Something to show");
+
+// console.log("hello Shelli");
+
+// console.table({ name: "shelli", age: 20 });
+
+//----------------------------------------
+//Declared variable...
+// let name;
+
+//Assign value...
+// let mango;
+// mango = "mango";
+// console.log(mango);
+//------------------------------------------
+
+//HomeWork1...........
+
+// var name = "shelli";
+// var whatIHaveTOBecome = "programmer";
+// var gender = "Female";
+// var twitterHandle = "palShelli";
+
+// console.log(name);
+// console.log(whatIHaveTOBecome);
+// console.log(gender);
+// console.log(twitterHandle);
+
+//-------------------------------------------------
+
+//homeWork2..............................
+// var firstFavNum = 4;
+// var secondFavNum = 8;
+
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** secondFavNum);
+
+//------------------------------------------
