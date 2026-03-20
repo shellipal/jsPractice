f2598fd98a582319adc3ba0bc5808093ecac33a2
shellipal <palshelli5@gmail.com>
2026-03-20 14:22:06 +0530

    conditional statements

---
+++ 02_Control_Flow/script.js
@@ -0,0 +1,41 @@
+//---------------conditional statements-------------
+// let a = 10;
+// let b = 20;
+
+// if (a > b) {
+//   console.log("a is greater then b");
+// } else if (a < b) {
+//   console.log("a is less then b");
+// } else {
+//   console.log("a is equal to b");
+// }
+
+// ..............................
+
+// let time = 10;
+// let greetings;
+
+// if (time < 10) {
+//   greetings = "Good Morning";
+//   console.log(greetings);
+// } else if (time < 20) {
+//   greetings = "Good Day";
+//   console.log(greetings);
+// } else {
+//   greetings = "Good Evening";
+//   console.log(greetings);
+// }
+
+// --------------------task------------------
+
+let password = 10;
+
+if (password === 8) {
+  console.log("welcome");
+} else if (password <= 8) {
+  console.log("password is too short");
+} else if (password >= 8) {
+  console.log("too long password & password should be 8 characters");
+} else {
+  console.log("please provide a password");
+}
