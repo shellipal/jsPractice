ae0dc1980684156525574596c75d5129efde10e5
shellipal <palshelli5@gmail.com>
2026-03-24 23:56:02 +0530

    array exercise

--- 03_Arrays/script.js
+++ 03_Arrays/script.js
@@ -7,5 +7,31 @@
 // const nums = [1, 2, 3, 4, 5];
 // console.log(nums);
 
-const stringArr = ["eat", "sleep", "code", "repet"];
-console.log(stringArr);
+// const stringArr = ["eat", "sleep", "code", "repet"];
+
+// Accessing items from our array.......
+
+// console.log(stringArr[0]);
+// console.log(stringArr[1]);
+// console.log(stringArr[2]);
+// console.log(stringArr[3]);
+
+//Nested Array or 2-D Array................
+// const nesArr = ["one", ["two", "three"], 1, true, false];
+// console.log(nesArr[1][1]);
+
+//-----exercise------------------------
+
+const favSingers = ["Dhanda", "BTS", "BlackPink"];
+console.log(favSingers[0]);
+
+const favNumbers = [2, 4, 8, 11];
+console.log(favNumbers);
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+// console.log(mixedArr);
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
