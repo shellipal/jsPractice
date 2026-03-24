fbaf07240189984081199433a20d1a310ed76b8d
shellipal <palshelli5@gmail.com>
2026-03-24 22:36:21 +0530

    array

---
+++ 03_Arrays/script.js
@@ -0,0 +1,11 @@
+//empty array...............
+
+// const myList = [];
+// console.log(myList);
+
+// ..........................
+// const nums = [1, 2, 3, 4, 5];
+// console.log(nums);
+
+const stringArr = ["eat", "sleep", "code", "repet"];
+console.log(stringArr);
