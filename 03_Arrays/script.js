63847759d671996199fb3ca8b29570c683323cb0
shellipal <palshelli5@gmail.com>
2026-04-04 22:07:19 +0530

    arrays methods

--- 03_Arrays/script.js
+++ 03_Arrays/script.js
@@ -22,16 +22,67 @@
 
 //-----exercise------------------------
 
-const favSingers = ["Dhanda", "BTS", "BlackPink"];
-console.log(favSingers[0]);
+// const favSingers = ["Dhanda", "BTS", "BlackPink"];
+// console.log(favSingers[0]);
 
-const favNumbers = [2, 4, 8, 11];
-console.log(favNumbers);
+// const favNumbers = [2, 4, 8, 11];
+// console.log(favNumbers);
 
-const mixedArr = ["string", ["otherarray"], 123, true];
+// const mixedArr = ["string", ["otherarray"], 123, true];
 
 // console.log(mixedArr);
-console.log(mixedArr[0]);
-console.log(mixedArr[1]);
-console.log(mixedArr[2]);
-console.log(mixedArr[3]);
+// console.log(mixedArr[0]);
+// console.log(mixedArr[1]);
+// console.log(mixedArr[2]);
+// console.log(mixedArr[3]);
+
+//----methods--------------
+
+const fruits = [
+  "apples",
+  "pomegranate",
+  "mango",
+  "strawberries",
+  "pineapple",
+  "grapes",
+];
+
+//----------push-------------------------
+console.log(fruits);
+// console.log(fruits.length);
+
+// fruits.push("Banana");
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+//-----------shift-------------------
+// fruits.shift();
+// console.log(fruits);
+
+// --------unshift-------------
+// fruits.unshift("orange");
+// console.log(fruits);
+
+// -----------concat----------
+
+// const newFruits = ["orange", "banana", "papaya"];
+
+// const totalFruits = fruits.concat(newFruits);
+// console.log(totalFruits);
+
+//--------------includes------------------
+const pl = ["js", "Golang", "python", "PHP"];
+const numbers = [3, 5, 2, 6, 7];
+
+// console.log(pl.includes("Golang"));
+// console.log(pl.join("-"));
+// console.log(pl.reverse());
+
+// -------------slice------------
+console.log(pl.slice(0, 2));
+
+//-------------sort-------------------
+console.log(numbers);
+console.log(numbers.sort());
