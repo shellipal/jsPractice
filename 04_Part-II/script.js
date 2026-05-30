52e631103735def545b825e3f7a3e7ac520407ec
shellipal <palshelli5@gmail.com>
2026-05-30 23:52:36 +0530

    template literals

---
+++ 04_Part-II/script.js
@@ -0,0 +1,27 @@
+//Template literals-----------
+console.log(`This is some random text`);
+
+// const firstName = "shelli";
+// const lastName = "pal";
+
+// console.log(`My name is : ${firstName} ${lastName}`);
+
+console.log(`${["one", "Two", "Three"]}`);
+
+//---------------------------------
+function info() {
+  return `My name is shelli pal & I'm 20 years old`;
+}
+
+console.log(`${info()}`);
+
+//-------Exercise--------------------
+console.log(`The quick
+brown fox
+jumps over
+the lazy dog`);
+
+const firstName = "Lakshay";
+const lastName = "Chaudhary";
+
+console.log(`Name is : ${firstName} ${lastName} `);
